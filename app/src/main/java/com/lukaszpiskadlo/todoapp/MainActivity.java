308401877706a5281e86920e8d9fc877124d9396
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-02-08 15:23:19 +0100

    Add toolbar and navigation drawer

--- app/src/main/java/com/lukaszpiskadlo/todoapp/MainActivity.java
+++ app/src/main/java/com/lukaszpiskadlo/todoapp/MainActivity.java
@@ -1,13 +1,58 @@
 package com.lukaszpiskadlo.todoapp;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        setDrawer(navView);
+    }
+
+    private void setDrawer(NavigationView navView) {
+        navView.setNavigationItemSelectedListener(item -> true);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close);
+
+        drawerLayout.addDrawerListener(drawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
